4d98e0852cd44c4943b9cb90c17c80a019ce2d0f
lozenia64 <lozenia64@gmail.com>
2018-10-02 22:28:31 +0900

    syntax conditional

--- syntax/conditional.js
+++ syntax/conditional.js
@@ -1,8 +1,10 @@
+var args = process.argv;
+console.log(args[2]);
 console.log('A');
 console.log('B');
-if(false){
-  console.log('C1'); //false (skip)
+if(args[2] === '1'){ //args == 1
+  console.log('C1'); //C1 (true)
 } else {
-  console.log('C2'); //true (run)
+  console.log('C2'); //C2 (false)
 }
 console.log('D');
