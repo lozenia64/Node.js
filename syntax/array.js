335a4a385e8128d0a91e571e7e09786670a4ae34
lozenia64 <lozenia64@gmail.com>
2018-10-02 23:49:39 +0900

    array

--- syntax/array.js
+++ syntax/array.js
@@ -1,3 +1,4 @@
+//Sample array
 var arr = ['A','B','C','D']
 console.log("arr : " + arr);
 console.log("length of arr : " + arr.length);
@@ -5,7 +6,19 @@ console.log("length of arr : " + arr.length);
 arr[3]='E'
 console.log("\narr[3] change to E\n");
 var i = 0;
-while(i<4){
+while(i < arr.length){
   console.log("arr[" + i + "] = " + arr[i]);
   i=i+1;
 }
+
+//Sample loop
+var brr = [15, 324, 89, 34, 7]
+console.log("\nbrr : " + brr);
+
+var i = 0;
+var sum = 0;
+while(i < brr.length){
+  sum = sum + brr[i];
+  i = i + 1;
+}
+console.log("Sum : " + sum);
